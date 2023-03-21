701323670cdb13bf8ae5b2a7948687a856e20272
shyxun <79041906+shyxun@users.noreply.github.com>
2023-03-21 20:30:07 +0700

    Update main.cpp
    Optimalisasi

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
 class Data
 {
 public:
+	string input_masuk = "";
+	bool mengandung_kata_kasar = false;
+	int jumlah_kata_kasar = 0;
+
 	vector<string> kata_kasar
 	{
 		"anjing",
@@ -20,13 +26,17 @@ public:
 		"bangsat",
 		"ngewe",
 		"lonte",
-		"asu"
+		"asu",
+		"anjg",
+		"asw",
+		"meki"
 	};
 
 	map<string, string> huruf_spesial
 	{
 		{"@", "a"},
 		{"1", "i"},
+		{"!", "i"},
 		{"0", "o"},
 		{"3", "e"},
 		{"6", "g"},
@@ -41,26 +51,27 @@ string olah_teks(string input, Data* data)
 	string hasil = input, dummy = input;
 
 	// mengubah teks menjadi lowercase.
-	for (char& lowercase : dummy)
-		lowercase = tolower(lowercase);
+	std::transform(dummy.begin(), dummy.end(), dummy.begin(), ::tolower);
 
 	// Melakukan cek jika kita menggunakan huruf spesial.
 	// 
 	// kita 'kutip' semua huruf spesial.
 	for (auto i = data->huruf_spesial.begin(); i != data->huruf_spesial.end(); i++)
 	{
-		// kita mengulang-mengulang lagi kode-nya sampai kita dapat mengubah huruf spesial menjadi huruf biasa.
-		for (int k = 0; k < dummy.size(); k++)
+		// cari posisi teks yang akan kita ubah.
+		size_t posisi = dummy.find(i->first);
+
+		// mencari teks yang ada karakter spesial-nya.
+		while (posisi != string::npos)
 		{
-			// saatnya kita mencari teks yang ada karakter spesial-nya.
-			if (dummy.find(i->first) != string::npos)
-			{
-				// Mengubah teks yang kita cari.
-				// argumen pertama berisi huruf yang akan kita cari, yaitu i->first, i->first adalah isi dari huruf_spesial yang dalam baris pertama.
-				// argumen kedua adalah panjangnya teks yang kita cari.
-				// argumen ketiga mengubah huruf spesial menjadi huruf normal.
-				dummy.replace(dummy.find(i->first), i->first.length(), i->second);
-			}
+			// Mengubah teks yang kita cari.
+			// argumen pertama berisi huruf yang akan kita cari, yaitu i->first, i->first adalah isi dari huruf_spesial yang dalam baris pertama.
+			// argumen kedua adalah panjangnya teks yang kita cari.
+			// argumen ketiga mengubah huruf spesial menjadi huruf normal.
+			dummy.replace(dummy.find(i->first), i->first.length(), i->second);
+
+			// Ubah posisi untuk menemukan karakter spesial selanjutnya.
+			posisi = dummy.find(i->first, posisi + 1);
 		}
 	}
 
@@ -75,6 +86,8 @@ string olah_teks(string input, Data* data)
 		// teks yang berada di-posisi saat ini adalah teks yang berbau kasar!
 		while (posisi != string::npos)
 		{
+			data->mengandung_kata_kasar = true;
+
 			// Mengubah teks yang kita cari.
 			// argumen pertama berisi huruf yang akan kita cari, yaitu 'posisi', yang berarti kita cari bahasa kasar bertahap-tahap.
 			// argumen kedua adalah panjangnya teks yang kita cari.
@@ -83,6 +96,7 @@ string olah_teks(string input, Data* data)
 
 			// Ubah posisi untuk menemukan kata kasar selanjutnya.
 			posisi = dummy.find(kata_kasar, posisi + 1);
+			data->jumlah_kata_kasar++;
 		}
 	}
 
@@ -95,21 +109,18 @@ int main()
 	Data* data = new Data;
 
 	// Informasi-informasi yang diperlukan.
-	string input_masuk;
-
+	
 	// Tahapan awal.
-	cout << "\x1B[31mKetik kata kasar dalam Bahasa Indonesia: \n";
-	getline(cin, input_masuk);
-
-	// pembatas.
-	cout << "\n";
+	cout << "\x1B[31mKetik kata kasar dalam Bahasa Indonesia:\n";
+	getline(cin, data->input_masuk);
 
 	// Melakukan pengolahan dari teks yang telah di-input.
-	cout << "\x1B[36mKata kasar yang telah di-sensor: \n";
-	cout << olah_teks(input_masuk, data);
+	cout << "\n\x1B[36mKata kasar yang telah di-sensor:\n";
+	cout << olah_teks(data->input_masuk, data) << ("\x1B[37m \n");;
+	cout << "\nApakah terdapat kata kasar: " << (data->mengandung_kata_kasar ? "ya, " : "tidak ") << (data->mengandung_kata_kasar ? (string("sebanyak: " + to_string(data->jumlah_kata_kasar))) : "") << "\n";
 
-	// mengubah warna menjadi warna putih.
-	cout << ("\x1B[37m \n");
+	// Menghapus "data" untuk menghindari memory leak.
+	delete data;
 
 	return 0;
 }
